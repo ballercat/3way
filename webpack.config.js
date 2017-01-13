2731e2a113af9d921cd4046779c2d5b9240968de
Arty Buldauskas <abuldauskas@wayfair.com>
2017-01-13 15:24:11 -0500

    fill in webpack config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+
+var config = {
+  context: path.join(__dirname, 'src'),
+  entry: [
+    './main.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'app'),
+    filename: 'bundle.js'
+  },
+  moduel: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel'],
+      },
+    ],
+  },
+  resolveLoader: {
+    root: [
+      path.join(__dirname, 'node_modules'),
+    ]
+  },
+  resolve: {
+    root: [
+      path.join(__dirname, 'node_modules'),
+    ],
+  }
+};
+module.exports = config;
+
